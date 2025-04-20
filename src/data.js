b9af14c47d807fbea27f1d820e74398f2f78dc27
komalkprajapati <komalprajapati230105@gmail.com>
2025-04-20 18:16:28 +0530

    add project

--- src/data.js
+++ src/data.js
@@ -8,13 +8,14 @@ const data =[
         image: "https://via.placeholder.com/150" // Replace with your project image URL
     },
     {
-        id: 1,
-        title: "To-Do App",
-        description: "A React-based to-do list application that allows users to add, edit, and delete tasks.",
-        githubLink: "https://github.com/komalkprajapati/iTask.git", // Replace with your GitHub link
-        liveDemoLink: "https://komalkprajapati.github.io/iTask/", // Replace with live demo URL
-        image: "https://via.placeholder.com/150" // Replace with your project image URL
-    }
+        id: 2,
+        title: "E-commerce App",
+        description: "A full-featured e-commerce site built with React, Redux, and React Router. Includes login, product list, cart, order flow, and profile management.",
+        githubLink: "https://github.com/komalkprajapati/Shoe-Store.git",
+        liveDemoLink: " https://komalkprajapati.github.io/Shoe-Store/",
+        image: "https://via.placeholder.com/150"
+      }
+      
 ]
 
 export default data;
