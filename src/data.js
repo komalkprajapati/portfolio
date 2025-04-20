b0fa1be24b9b7e02f680a83b2c063bc9538be437
komalkprajapati <komalprajapati230105@gmail.com>
2025-04-20 17:37:35 +0530

    add data.js file

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const data =[
+    {
+        id: 1,
+        title: "To-Do App",
+        description: "A React-based to-do list application that allows users to add, edit, and delete tasks.",
+        githubLink: "https://github.com/komalkprajapati/iTask.git", // Replace with your GitHub link
+        liveDemoLink: "https://komalkprajapati.github.io/iTask/", // Replace with live demo URL
+        image: "https://via.placeholder.com/150" // Replace with your project image URL
+    },
+    {
+        id: 1,
+        title: "To-Do App",
+        description: "A React-based to-do list application that allows users to add, edit, and delete tasks.",
+        githubLink: "https://github.com/komalkprajapati/iTask.git", // Replace with your GitHub link
+        liveDemoLink: "https://komalkprajapati.github.io/iTask/", // Replace with live demo URL
+        image: "https://via.placeholder.com/150" // Replace with your project image URL
+    }
+]
+
+export default data;
